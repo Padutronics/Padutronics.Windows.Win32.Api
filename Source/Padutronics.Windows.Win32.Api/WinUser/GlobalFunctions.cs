2d4b5e5ed661e356dd542edcf405b310b5e01067
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-11 21:58:33 +0100

    Add function WinUser.GlobalFunctions.CreateWindowExW

--- Source/Padutronics.Windows.Win32.Api/WinUser/GlobalFunctions.cs
+++ Source/Padutronics.Windows.Win32.Api/WinUser/GlobalFunctions.cs
@@ -6,6 +6,9 @@ namespace Padutronics.Windows.Win32.Api.WinUser;
 public static class GlobalFunctions
 {
     [DllImport(DllNames.User32, CharSet = CharSet.Unicode, SetLastError = true)]
+    public static extern IntPtr CreateWindowExW(WS_EX dwExStyle, IntPtr lpClassName, string lpWindowName, WS dwStyle, int X, int Y, int nWidth, int nHeight, IntPtr hWndParent, IntPtr hMenu, IntPtr hInstance, IntPtr lpParam);
+
+    [DllImport(DllNames.User32, CharSet = CharSet.Unicode, SetLastError = true)]
     public static extern IntPtr LoadCursorW(IntPtr hInstance, IntPtr lpCursorName);
 
     [DllImport(DllNames.User32, CharSet = CharSet.Unicode, SetLastError = true)]
