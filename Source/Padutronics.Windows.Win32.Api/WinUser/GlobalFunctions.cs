459d6dff1026251c1dbe38f6437d28732a16f4c9
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-11 22:01:01 +0100

    Add function WinUser.GlobalFunctions.GetSystemMetrics

--- Source/Padutronics.Windows.Win32.Api/WinUser/GlobalFunctions.cs
+++ Source/Padutronics.Windows.Win32.Api/WinUser/GlobalFunctions.cs
@@ -12,6 +12,9 @@ public static class GlobalFunctions
     [DllImport(DllNames.User32, CharSet = CharSet.Unicode, SetLastError = true)]
     public static extern IntPtr CreateWindowExW(WS_EX dwExStyle, IntPtr lpClassName, string lpWindowName, WS dwStyle, int X, int Y, int nWidth, int nHeight, IntPtr hWndParent, IntPtr hMenu, IntPtr hInstance, IntPtr lpParam);
 
+    [DllImport(DllNames.User32, SetLastError = true)]
+    public static extern int GetSystemMetrics(SM nIndex);
+
     [DllImport(DllNames.User32, CharSet = CharSet.Unicode, SetLastError = true)]
     public static extern IntPtr LoadCursorW(IntPtr hInstance, IntPtr lpCursorName);
 
