072c9978207eafbdcfa4054ff99ae23cd7deb2cb
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-11 22:01:24 +0100

    Add enumeration WinUser.SIZE

---
+++ Source/Padutronics.Windows.Win32.Api/WinUser/SIZE.cs
@@ -0,0 +1,10 @@
+namespace Padutronics.Windows.Win32.Api.WinUser;
+
+public enum SIZE
+{
+    SIZE_RESTORED = 0,
+    SIZE_MINIMIZED = 1,
+    SIZE_MAXIMIZED = 2,
+    SIZE_MAXSHOW = 3,
+    SIZE_MAXHIDE = 4
+}
