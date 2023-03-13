82dc6e1f6c0475609863ab9d2776b972a2ee446e
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-13 10:49:08 +0100

    Add enumeration WinUser.MK

---
+++ Source/Padutronics.Windows.Win32.Api/WinUser/MK.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Padutronics.Windows.Win32.Api.WinUser;
+
+[Flags]
+public enum MK
+{
+    MK_LBUTTON = 0x0001,
+    MK_RBUTTON = 0x0002,
+    MK_SHIFT = 0x0004,
+    MK_CONTROL = 0x0008,
+    MK_MBUTTON = 0x0010,
+    MK_XBUTTON1 = 0x0020,
+    MK_XBUTTON2 = 0x0040
+}
