50664d2e5a9c2cb7f990580c41cccc3ed31f56fb
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-14 10:56:24 +0100

    Add function DWrite.GlobalFunctions.DWriteCreateFactory

---
+++ Source/Padutronics.Windows.Win32.Api/DWrite/GlobalFunctions.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Padutronics.Windows.Win32.Api.DWrite;
+
+public static class GlobalFunctions
+{
+    [DllImport(DllNames.DWrite, PreserveSig = false)]
+    public static extern void DWriteCreateFactory(DWRITE_FACTORY_TYPE factoryType, ref Guid iid, out IntPtr factory);
+}
--- Source/Padutronics.Windows.Win32.Api/DllNames.cs
+++ Source/Padutronics.Windows.Win32.Api/DllNames.cs
@@ -4,6 +4,7 @@ internal static class DllNames
 {
     public const string CWrapper = "Padutronics.Windows.Win32.Api.CWrapper.dll";
     public const string D2D1 = "D2D1.dll";
+    public const string DWrite = "Dwrite.dll";
     public const string Dxgi = "DXGI.dll";
     public const string User32 = "User32.dll";
 }
