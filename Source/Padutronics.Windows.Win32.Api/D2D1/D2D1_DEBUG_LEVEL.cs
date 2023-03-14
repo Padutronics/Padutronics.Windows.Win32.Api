112d9649923cc1278b93645c7067a961ad9c7c25
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-14 10:48:16 +0100

    Add enumeration D2D1.D2D1_DEBUG_LEVEL

---
+++ Source/Padutronics.Windows.Win32.Api/D2D1/D2D1_DEBUG_LEVEL.cs
@@ -0,0 +1,9 @@
+namespace Padutronics.Windows.Win32.Api.D2D1;
+
+public enum D2D1_DEBUG_LEVEL
+{
+    D2D1_DEBUG_LEVEL_NONE = 0,
+    D2D1_DEBUG_LEVEL_ERROR = 1,
+    D2D1_DEBUG_LEVEL_WARNING = 2,
+    D2D1_DEBUG_LEVEL_INFORMATION = 3
+}
