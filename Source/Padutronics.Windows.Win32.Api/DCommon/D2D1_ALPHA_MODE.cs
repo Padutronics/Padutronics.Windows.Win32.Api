a2cfaa6ac06ea4b763876bc2a4913165d69e5691
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-15 12:57:45 +0100

    Add enumeration DCommon.D2D1_ALPHA_MODE

---
+++ Source/Padutronics.Windows.Win32.Api/DCommon/D2D1_ALPHA_MODE.cs
@@ -0,0 +1,9 @@
+namespace Padutronics.Windows.Win32.Api.DCommon;
+
+public enum D2D1_ALPHA_MODE
+{
+    D2D1_ALPHA_MODE_UNKNOWN = 0,
+    D2D1_ALPHA_MODE_PREMULTIPLIED = 1,
+    D2D1_ALPHA_MODE_STRAIGHT = 2,
+    D2D1_ALPHA_MODE_IGNORE = 3
+}
