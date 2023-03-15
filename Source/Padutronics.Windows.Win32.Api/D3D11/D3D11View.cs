0d50fcd2c15c7d02860fd61525ec7694562e04e6
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-15 11:23:46 +0100

    Add interface D3D11.ID3D11View

---
+++ Source/Padutronics.Windows.Win32.Api/D3D11/D3D11View.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Padutronics.Windows.Win32.Api.D3D11;
+
+public class D3D11View : D3D11DeviceChild, ID3D11View
+{
+    public D3D11View(IntPtr pointer) :
+        base(pointer)
+    {
+    }
+}
---
+++ Source/Padutronics.Windows.Win32.Api/D3D11/ID3D11View.cs
@@ -0,0 +1,8 @@
+using System.Runtime.InteropServices;
+
+namespace Padutronics.Windows.Win32.Api.D3D11;
+
+[Guid("839d1216-bb2e-412b-b7f4-a9dbebe08ed1")]
+public interface ID3D11View : ID3D11DeviceChild
+{
+}
