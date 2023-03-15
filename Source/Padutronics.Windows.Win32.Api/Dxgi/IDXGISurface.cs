bed32bd7104096c1c447d1ef3750a8c0f88bbb81
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-15 11:27:52 +0100

    Add interface Dxgi.IDXGISurface

---
+++ Source/Padutronics.Windows.Win32.Api/Dxgi/IDXGISurface.cs
@@ -0,0 +1,8 @@
+using System.Runtime.InteropServices;
+
+namespace Padutronics.Windows.Win32.Api.Dxgi;
+
+[Guid("cafcb56c-6ac3-4889-bf47-9e23bbd260ec")]
+public interface IDXGISurface : IDXGIDeviceSubObject
+{
+}
