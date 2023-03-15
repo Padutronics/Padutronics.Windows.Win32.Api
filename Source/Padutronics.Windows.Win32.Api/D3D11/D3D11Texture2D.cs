9c3870f65c020aefacf6afb902872b1ca8fdf078
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-15 11:25:29 +0100

    Add interface D3D11.ID3D11Texture2D

---
+++ Source/Padutronics.Windows.Win32.Api/D3D11/D3D11Texture2D.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Padutronics.Windows.Win32.Api.D3D11;
+
+public class D3D11Texture2D : D3D11Resource, ID3D11Texture2D
+{
+    public D3D11Texture2D(IntPtr pointer) :
+        base(pointer)
+    {
+    }
+}
---
+++ Source/Padutronics.Windows.Win32.Api/D3D11/ID3D11Texture2D.cs
@@ -0,0 +1,8 @@
+using System.Runtime.InteropServices;
+
+namespace Padutronics.Windows.Win32.Api.D3D11;
+
+[Guid("6f15aaf2-d208-4e89-9ab4-489535d34f9c")]
+public interface ID3D11Texture2D : ID3D11Resource
+{
+}
