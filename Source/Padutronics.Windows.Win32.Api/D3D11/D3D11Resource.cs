68342fa0a57b1697bfdc059f586c23cb42411bdb
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-15 11:25:05 +0100

    Add interface D3D11.ID3D11Resource

---
+++ Source/Padutronics.Windows.Win32.Api/D3D11/D3D11Resource.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Padutronics.Windows.Win32.Api.D3D11;
+
+public class D3D11Resource : D3D11DeviceChild, ID3D11Resource
+{
+    public D3D11Resource(IntPtr pointer) :
+        base(pointer)
+    {
+    }
+}
---
+++ Source/Padutronics.Windows.Win32.Api/D3D11/ID3D11Resource.cs
@@ -0,0 +1,8 @@
+using System.Runtime.InteropServices;
+
+namespace Padutronics.Windows.Win32.Api.D3D11;
+
+[Guid("dc8e63f3-d12b-4952-b47b-5e45026a862d")]
+public interface ID3D11Resource : ID3D11DeviceChild
+{
+}
