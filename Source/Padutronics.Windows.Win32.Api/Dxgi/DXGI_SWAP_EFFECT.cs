0d16a73ff9099fa024663b7380d46c1c40c169c3
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-15 11:50:17 +0100

    Add enumeration Dxgi.DXGI_SWAP_EFFECT

---
+++ Source/Padutronics.Windows.Win32.Api/Dxgi/DXGI_SWAP_EFFECT.cs
@@ -0,0 +1,9 @@
+namespace Padutronics.Windows.Win32.Api.Dxgi;
+
+public enum DXGI_SWAP_EFFECT
+{
+    DXGI_SWAP_EFFECT_DISCARD = 0,
+    DXGI_SWAP_EFFECT_SEQUENTIAL = 1,
+    DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL = 3,
+    DXGI_SWAP_EFFECT_FLIP_DISCARD = 4
+}
