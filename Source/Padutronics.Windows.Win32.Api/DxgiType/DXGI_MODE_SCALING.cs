0660c563ac11ca29dd6cb111f11733b2d7175ed7
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-15 11:53:25 +0100

    Add enumeration DxgiType.DXGI_MODE_SCALING

---
+++ Source/Padutronics.Windows.Win32.Api/DxgiType/DXGI_MODE_SCALING.cs
@@ -0,0 +1,8 @@
+namespace Padutronics.Windows.Win32.Api.DxgiType;
+
+public enum DXGI_MODE_SCALING
+{
+    DXGI_MODE_SCALING_UNSPECIFIED = 0,
+    DXGI_MODE_SCALING_CENTERED = 1,
+    DXGI_MODE_SCALING_STRETCHED = 2
+}
