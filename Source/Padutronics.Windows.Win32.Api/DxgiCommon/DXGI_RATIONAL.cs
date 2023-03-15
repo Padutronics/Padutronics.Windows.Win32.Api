d361e0ec781e64396fa94b562305d371ce98a213
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-15 11:54:48 +0100

    Add structure DxgiCommon.DXGI_RATIONAL

---
+++ Source/Padutronics.Windows.Win32.Api/DxgiCommon/DXGI_RATIONAL.cs
@@ -0,0 +1,7 @@
+namespace Padutronics.Windows.Win32.Api.DxgiCommon;
+
+public struct DXGI_RATIONAL
+{
+    public int Numerator;
+    public int Denominator;
+}
---
+++ Tests/Padutronics.Windows.Win32.Api.Tests/DxgiCommon/Structure_size_is_correct.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using Padutronics.Windows.Win32.Api.DxgiCommon;
+using System.Runtime.InteropServices;
+
+namespace Padutronics.Windows.Win32.Api.Tests.DxgiCommon;
+
+[TestFixture]
+internal sealed class Structure_size_is_correct
+{
+    [Test]
+    public void Size_of_DXGI_RATIONAL()
+    {
+        Assert.That(Marshal.SizeOf(typeof(DXGI_RATIONAL)), Is.EqualTo(8));
+    }
+}
