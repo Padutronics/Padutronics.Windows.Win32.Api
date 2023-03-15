4175de54f31d71f40e5eceb644bbabdb36c11a5b
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-15 12:24:38 +0100

    Add constants WinError.DXGI_ERROR

---
+++ Source/Padutronics.Windows.Win32.Api/WinError/DXGI_ERROR.cs
@@ -0,0 +1,37 @@
+namespace Padutronics.Windows.Win32.Api.WinError;
+
+public static class DXGI_ERROR
+{
+    public const int DXGI_ERROR_INVALID_CALL = unchecked((int)0x887A0001);
+    public const int DXGI_ERROR_NOT_FOUND = unchecked((int)0x887A0002);
+    public const int DXGI_ERROR_MORE_DATA = unchecked((int)0x887A0003);
+    public const int DXGI_ERROR_UNSUPPORTED = unchecked((int)0x887A0004);
+    public const int DXGI_ERROR_DEVICE_REMOVED = unchecked((int)0x887A0005);
+    public const int DXGI_ERROR_DEVICE_HUNG = unchecked((int)0x887A0006);
+    public const int DXGI_ERROR_DEVICE_RESET = unchecked((int)0x887A0007);
+    public const int DXGI_ERROR_WAS_STILL_DRAWING = unchecked((int)0x887A000A);
+    public const int DXGI_ERROR_FRAME_STATISTICS_DISJOINT = unchecked((int)0x887A000B);
+    public const int DXGI_ERROR_GRAPHICS_VIDPN_SOURCE_IN_USE = unchecked((int)0x887A000C);
+    public const int DXGI_ERROR_DRIVER_INTERNAL_ERROR = unchecked((int)0x887A0020);
+    public const int DXGI_ERROR_NONEXCLUSIVE = unchecked((int)0x887A0021);
+    public const int DXGI_ERROR_NOT_CURRENTLY_AVAILABLE = unchecked((int)0x887A0022);
+    public const int DXGI_ERROR_REMOTE_CLIENT_DISCONNECTED = unchecked((int)0x887A0023);
+    public const int DXGI_ERROR_REMOTE_OUTOFMEMORY = unchecked((int)0x887A0024);
+    public const int DXGI_ERROR_ACCESS_LOST = unchecked((int)0x887A0026);
+    public const int DXGI_ERROR_WAIT_TIMEOUT = unchecked((int)0x887A0027);
+    public const int DXGI_ERROR_SESSION_DISCONNECTED = unchecked((int)0x887A0028);
+    public const int DXGI_ERROR_RESTRICT_TO_OUTPUT_STALE = unchecked((int)0x887A0029);
+    public const int DXGI_ERROR_CANNOT_PROTECT_CONTENT = unchecked((int)0x887A002A);
+    public const int DXGI_ERROR_ACCESS_DENIED = unchecked((int)0x887A002B);
+    public const int DXGI_ERROR_NAME_ALREADY_EXISTS = unchecked((int)0x887A002C);
+    public const int DXGI_ERROR_SDK_COMPONENT_MISSING = unchecked((int)0x887A002D);
+    public const int DXGI_ERROR_NOT_CURRENT = unchecked((int)0x887A002E);
+    public const int DXGI_ERROR_HW_PROTECTION_OUTOFMEMORY = unchecked((int)0x887A0030);
+    public const int DXGI_ERROR_DYNAMIC_CODE_POLICY_VIOLATION = unchecked((int)0x887A0031);
+    public const int DXGI_ERROR_NON_COMPOSITED_UI = unchecked((int)0x887A0032);
+    public const int DXGI_ERROR_MODE_CHANGE_IN_PROGRESS = unchecked((int)0x887A0025);
+    public const int DXGI_ERROR_CACHE_CORRUPT = unchecked((int)0x887A0033);
+    public const int DXGI_ERROR_CACHE_FULL = unchecked((int)0x887A0034);
+    public const int DXGI_ERROR_CACHE_HASH_COLLISION = unchecked((int)0x887A0035);
+    public const int DXGI_ERROR_ALREADY_EXISTS = unchecked((int)0x887A0036);
+}
