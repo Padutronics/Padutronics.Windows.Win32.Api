ac976aaa81f7291b22406effaaf1d6b5f022656c
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-16 11:35:32 +0100

    Add constants WinError.DXGI_STATUS

---
+++ Source/Padutronics.Windows.Win32.Api/WinError/DXGI_STATUS.cs
@@ -0,0 +1,15 @@
+namespace Padutronics.Windows.Win32.Api.WinError;
+
+public static class DXGI_STATUS
+{
+    public const int DXGI_STATUS_OCCLUDED = 0x087A0001;
+    public const int DXGI_STATUS_CLIPPED = 0x087A0002;
+    public const int DXGI_STATUS_NO_REDIRECTION = 0x087A0004;
+    public const int DXGI_STATUS_NO_DESKTOP_ACCESS = 0x087A0005;
+    public const int DXGI_STATUS_GRAPHICS_VIDPN_SOURCE_IN_USE = 0x087A0006;
+    public const int DXGI_STATUS_MODE_CHANGED = 0x087A0007;
+    public const int DXGI_STATUS_MODE_CHANGE_IN_PROGRESS = 0x087A0008;
+    public const int DXGI_STATUS_UNOCCLUDED = 0x087A0009;
+    public const int DXGI_STATUS_DDA_WAS_STILL_DRAWING = 0x087A000A;
+    public const int DXGI_STATUS_PRESENT_REQUIRED = 0x087A002F;
+}
