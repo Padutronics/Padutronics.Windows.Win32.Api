58b2b6b6f65b3f71d999beac95f45c6b057e0b0d
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-16 12:16:51 +0100

    Add structure DCommon.D2D_SIZE_U

---
+++ Source/Padutronics.Windows.Win32.Api/DCommon/D2D_SIZE_U.cs
@@ -0,0 +1,7 @@
+namespace Padutronics.Windows.Win32.Api.DCommon;
+
+public struct D2D_SIZE_U
+{
+    public int width;
+    public int height;
+}
--- Tests/Padutronics.Windows.Win32.Api.Tests/DCommon/Structure_size_is_correct.cs
+++ Tests/Padutronics.Windows.Win32.Api.Tests/DCommon/Structure_size_is_correct.cs
@@ -8,6 +8,12 @@ namespace Padutronics.Windows.Win32.Api.Tests.DCommon;
 internal sealed class Structure_size_is_correct
 {
     [Test]
+    public void Size_of_D2D_SIZE_U()
+    {
+        Assert.That(Marshal.SizeOf(typeof(D2D_SIZE_U)), Is.EqualTo(8));
+    }
+
+    [Test]
     public void Size_of_D2D1_PIXEL_FORMAT()
     {
         Assert.That(Marshal.SizeOf(typeof(D2D1_PIXEL_FORMAT)), Is.EqualTo(8));
