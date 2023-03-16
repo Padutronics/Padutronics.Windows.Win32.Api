cb76c90354b1f4fbe5af144a6f40365f50219586
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-16 12:27:55 +0100

    Add structure D2D1.D2D1_BRUSH_PROPERTIES

---
+++ Source/Padutronics.Windows.Win32.Api/D2D1/D2D1_BRUSH_PROPERTIES.cs
@@ -0,0 +1,9 @@
+using Padutronics.Windows.Win32.Api.DCommon;
+
+namespace Padutronics.Windows.Win32.Api.D2D1;
+
+public struct D2D1_BRUSH_PROPERTIES
+{
+    public float opacity;
+    public D2D_MATRIX_3X2_F transform;
+}
