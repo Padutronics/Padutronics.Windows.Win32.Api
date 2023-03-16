638aa7304e53ecd836575a8654315c9b126be8e9
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-16 12:09:40 +0100

    Add interface D2D1.ID2D1RadialGradientBrush

---
+++ Source/Padutronics.Windows.Win32.Api/D2D1/D2D1RadialGradientBrush.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Padutronics.Windows.Win32.Api.D2D1;
+
+public class D2D1RadialGradientBrush : D2D1Brush, ID2D1RadialGradientBrush
+{
+    public D2D1RadialGradientBrush(IntPtr pointer) :
+        base(pointer)
+    {
+    }
+}
