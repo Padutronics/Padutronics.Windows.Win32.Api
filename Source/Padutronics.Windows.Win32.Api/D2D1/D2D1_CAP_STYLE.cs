02741d9c3d29b0f3aa04196745e68da0ab26b24a
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-17 14:13:30 +0100

    Add enumeration D2D1.D2D1_CAP_STYLE

---
+++ Source/Padutronics.Windows.Win32.Api/D2D1/D2D1_CAP_STYLE.cs
@@ -0,0 +1,9 @@
+namespace Padutronics.Windows.Win32.Api.D2D1;
+
+public enum D2D1_CAP_STYLE
+{
+    D2D1_CAP_STYLE_FLAT = 0,
+    D2D1_CAP_STYLE_SQUARE = 1,
+    D2D1_CAP_STYLE_ROUND = 2,
+    D2D1_CAP_STYLE_TRIANGLE = 3
+}
