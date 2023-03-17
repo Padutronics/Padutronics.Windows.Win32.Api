013cfc44b7833f272ead4d6cd980bab847ae5a1b
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-17 14:09:05 +0100

    Add interface D2D1.ID2D1StrokeStyle

---
+++ Source/Padutronics.Windows.Win32.Api/D2D1/D2D1StrokeStyle.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Padutronics.Windows.Win32.Api.D2D1;
+
+public class D2D1StrokeStyle : D2D1Resource, ID2D1StrokeStyle
+{
+    public D2D1StrokeStyle(IntPtr pointer) :
+        base(pointer)
+    {
+    }
+}
