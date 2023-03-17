9cf82d1b64761310b989334723a976c290192fc1
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-17 14:05:18 +0100

    Add interface DWrite.IDWriteFontCollection

---
+++ Source/Padutronics.Windows.Win32.Api/DWrite/IDWriteFontCollection.cs
@@ -0,0 +1,9 @@
+using Padutronics.Windows.Win32.Api.Unknwn;
+using System.Runtime.InteropServices;
+
+namespace Padutronics.Windows.Win32.Api.DWrite;
+
+[Guid("a84cee02-3eea-4eee-a827-87c1a02a0fcc")]
+public interface IDWriteFontCollection : IUnknown
+{
+}
