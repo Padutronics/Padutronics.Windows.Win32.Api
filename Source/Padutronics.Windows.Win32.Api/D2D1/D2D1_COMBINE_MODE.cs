e2f5004af3cd8cfa4cce876172cfad0ca0bf5bf9
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-17 14:42:19 +0100

    Add enumeration D2D1.D2D1_COMBINE_MODE

---
+++ Source/Padutronics.Windows.Win32.Api/D2D1/D2D1_COMBINE_MODE.cs
@@ -0,0 +1,9 @@
+namespace Padutronics.Windows.Win32.Api.D2D1;
+
+public enum D2D1_COMBINE_MODE
+{
+    D2D1_COMBINE_MODE_UNION = 0,
+    D2D1_COMBINE_MODE_INTERSECT = 1,
+    D2D1_COMBINE_MODE_XOR = 2,
+    D2D1_COMBINE_MODE_EXCLUDE = 3
+}
