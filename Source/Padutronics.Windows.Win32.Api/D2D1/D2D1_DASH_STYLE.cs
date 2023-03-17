076f5e40d70ae5891752ccb0ad543fe2c6d64001
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-17 14:14:11 +0100

    Add enumeration D2D1.D2D1_DASH_STYLE

---
+++ Source/Padutronics.Windows.Win32.Api/D2D1/D2D1_DASH_STYLE.cs
@@ -0,0 +1,11 @@
+namespace Padutronics.Windows.Win32.Api.D2D1;
+
+public enum D2D1_DASH_STYLE
+{
+    D2D1_DASH_STYLE_SOLID = 0,
+    D2D1_DASH_STYLE_DASH = 1,
+    D2D1_DASH_STYLE_DOT = 2,
+    D2D1_DASH_STYLE_DASH_DOT = 3,
+    D2D1_DASH_STYLE_DASH_DOT_DOT = 4,
+    D2D1_DASH_STYLE_CUSTOM = 5
+}
