a5eb9ac35805d994abe38b471ddc1cbcfca2e0f0
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-17 14:13:52 +0100

    Add enumeration D2D1.D2D1_LINE_JOIN

---
+++ Source/Padutronics.Windows.Win32.Api/D2D1/D2D1_LINE_JOIN.cs
@@ -0,0 +1,9 @@
+namespace Padutronics.Windows.Win32.Api.D2D1;
+
+public enum D2D1_LINE_JOIN
+{
+    D2D1_LINE_JOIN_MITER = 0,
+    D2D1_LINE_JOIN_BEVEL = 1,
+    D2D1_LINE_JOIN_ROUND = 2,
+    D2D1_LINE_JOIN_MITER_OR_BEVEL = 3
+}
