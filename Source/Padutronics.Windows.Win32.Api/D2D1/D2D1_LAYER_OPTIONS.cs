a5847870e1efd42bded5529dfbe3ad8178ccb66c
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-20 14:58:38 +0100

    Add enumeration D2D1.D2D1_LAYER_OPTIONS

---
+++ Source/Padutronics.Windows.Win32.Api/D2D1/D2D1_LAYER_OPTIONS.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Padutronics.Windows.Win32.Api.D2D1;
+
+[Flags]
+public enum D2D1_LAYER_OPTIONS
+{
+    D2D1_LAYER_OPTIONS_NONE = 0x00000000,
+    D2D1_LAYER_OPTIONS_INITIALIZE_FOR_CLEARTYPE = 0x00000001
+}
