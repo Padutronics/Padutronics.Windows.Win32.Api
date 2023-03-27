0e49e6753efeed8cf7f725b618a501bb2b77cec5
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-27 12:31:49 +0200

    Add structure WinUser.MINMAXINFO

---
+++ Source/Padutronics.Windows.Win32.Api/WinUser/MINMAXINFO.cs
@@ -0,0 +1,12 @@
+﻿using Padutronics.Windows.Win32.Api.WinDef;
+
+namespace Padutronics.Windows.Win32.Api.WinUser;
+
+public struct MINMAXINFO
+{
+    public POINT ptReserved;
+    public POINT ptMaxSize;
+    public POINT ptMaxPosition;
+    public POINT ptMinTrackSize;
+    public POINT ptMaxTrackSize;
+}
