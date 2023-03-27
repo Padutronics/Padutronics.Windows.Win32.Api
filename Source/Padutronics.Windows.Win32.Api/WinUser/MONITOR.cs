3e539bbbeb7b6e0bcf07fda43324387b3d1098e0
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-27 12:32:38 +0200

    Add enumeration WinUser.MONITOR

---
+++ Source/Padutronics.Windows.Win32.Api/WinUser/MONITOR.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Padutronics.Windows.Win32.Api.WinUser;
+
+[Flags]
+public enum MONITOR
+{
+    MONITOR_DEFAULTTONULL = 0x00000000,
+    MONITOR_DEFAULTTOPRIMARY = 0x00000001,
+    MONITOR_DEFAULTTONEAREST = 0x00000002
+}
